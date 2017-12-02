7b3e35bd9ee064bbbdb5ee55ed442a9abe4310fc
BadranYahyaoui <mohamedbadran.yahyaoui@esprit.tn>
2017-12-02 14:58:06 +0100

    adding product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {Form, FormsModule} from "@angular/forms"
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import {ConvertToSpacesPipe} from "./myCustomPipes/convert-to-spaces";
+import { StarComponent } from './star/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,ProductsComponent]
 })
 export class AppModule { }
