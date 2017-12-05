98933fde58f00354a81d7a8f12a0e24a3ffa9a08
BadranYahyaoui <mohamedbadran.yahyaoui@esprit.tn>
2017-12-05 22:39:57 +0100

    adding sheets  files

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {ProductsComponent} from "./products.component";
 import {ProductDetailComponent} from "./product-detail.component";
-import {StarComponent} from "../star/star.component";
 import {ConvertToSpacesPipe} from "../myCustomPipes/convert-to-spaces";
-import {FormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
 import {ProductService} from "./product.service";
 import {ProductGuardService} from "./product-guard.service";
